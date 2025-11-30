a4f69b38d48ab7ca49f5dc114227810d254127b8
Christoffer Dam-Hansen <christoffer.dam.hansen@gmail.com>
2025-11-30 12:06:11 +0100

    updated test helper expander

--- src/test/scala/expanderTest.scala
+++ src/test/scala/expanderTest.scala
@@ -7,13 +7,17 @@ class ExpanderTest extends AnyFlatSpec with ChiselScalatestTester {
 
   behavior of "Expander"
 
-  private def testHelper(message: String, double: Boolean): TestResult = {
+  private def testHelper(
+      message: String,
+      double: Boolean,
+      radix: Int
+  ): TestResult = {
     test(new Expander(double)) { dut =>
       // "hello there" in binary (88 bits = 11 bytes)
       val pwBinary = message
 
       val bytes = pwBinary.length / 8
-      val msg = BigInt(pwBinary)
+      val msg = BigInt(pwBinary, radix)
       val blockExp = expectedBlock(bytes, msg)
       val Wexp = expectedW(blockExp)
 
@@ -53,14 +57,16 @@ class ExpanderTest extends AnyFlatSpec with ChiselScalatestTester {
   it should "calculate the right w[0..63] from the message 'Hello there' in binary" in {
     testHelper(
       "0110100001100101011011000110110001101111001000000111010001101000011001010111001001100101",
-      false
+      false,
+      2
     )
   }
 
   it should "calculate w[0..63] from the message 'Hello there', in 25 cycles" in {
     testHelper(
       "0110100001100101011011000110110001101111001000000111010001101000011001010111001001100101",
-      true
+      true,
+      2
     )
   }
 
@@ -68,7 +74,8 @@ class ExpanderTest extends AnyFlatSpec with ChiselScalatestTester {
     Seq(true, false).foreach { w =>
       testHelper(
         "001",
-        w
+        w,
+        2
       )
     }
   }
@@ -77,7 +84,8 @@ class ExpanderTest extends AnyFlatSpec with ChiselScalatestTester {
     Seq(true, false).foreach { w =>
       testHelper(
         "0",
-        w
+        w,
+        2
       )
     }
   }
@@ -86,7 +94,8 @@ class ExpanderTest extends AnyFlatSpec with ChiselScalatestTester {
     Seq(true, false).foreach { w =>
       testHelper(
         "000000",
-        w
+        w,
+        2
       )
     }
   }
@@ -95,7 +104,17 @@ class ExpanderTest extends AnyFlatSpec with ChiselScalatestTester {
     Seq(true, false).foreach { w =>
       testHelper(
         "11111",
-        w
+        w,
+        2
+      )
+    }
+  }
+  it should "calculate w[0..63] from message 'abc'" in {
+    Seq(true, false).foreach { w =>
+      testHelper(
+        "abc",
+        w,
+        16
       )
     }
   }
