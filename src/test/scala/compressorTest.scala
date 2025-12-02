605d4bcd2fcd4708c7ee8a554b774837b80c07cd
Christoffer Dam-Hansen <christoffer.dam.hansen@gmail.com>
2025-12-02 12:25:52 +0100

    Add a new workflow, and update compressortest

--- src/test/scala/compressorTest.scala
+++ src/test/scala/compressorTest.scala
@@ -40,11 +40,46 @@ class compressorTest extends AnyFlatSpec with ChiselScalatestTester {
       assert(hw == sw, s"Hardware: $hw, Software: $sw")
     }
   }
+
   it should "calculate the correct block from random value" in {
-    test(new compressor) { dut => 
-      val message = "000ab0c83333333300000000000000000000000000000000000000000000000000000000000000000000000000000000000000aaaaaaa0000000001"
-      expectedBlockCyclic(message).foreach(block => println(f"${block.toString(16).reverse.padTo(128,'0').reverse}"))
-    
+    test(new compressor) { dut =>
+      val message =
+        "0ab0c83333333300000000000000000000000000000000000000000000000000000000000000000000000000000000000000aaaaaaa000000001"
+      val blocksExp = expectedBlockCyclic(message)
+      val byteLength = message.length
+      dut.io.reset_hash.poke(true.B)
+      dut.clock.step()
+      dut.io.reset_hash.poke(false.B)
+      for (i <- 0 until blocksExp.length) {
+        val blockExp = blocksExp(i)
+        println(blockExp.toString(16).reverse.padTo(128, '0').reverse)
+        val wExp = expectedW(blockExp)
+
+        // inject w[0..63] to compressor
+        for (i <- 0 until 64) {
+          dut.io.w(i).poke(wExp(i).U(32.W))
+        }
+
+        dut.io.enable.poke(true.B)
+        // start compression
+        dut.clock.step()
+
+        // run clock until the finished bit is high
+        while (!dut.io.finished.peek().litToBoolean) {
+          dut.clock.step()
+        }
+        dut.io.enable.poke(false.B)
+        dut.clock.step()
+      }
+
+      // collect hash output
+      val hw =
+        hashToHex((0 until 8).map(i => dut.io.hash_out(i).peek().litValue))
+      // from hex string to byte array
+      val swMessage = BigInt(message, 16).toByteArray
+      val sw = goldenSha256(swMessage)
+      assert(hw == sw, s"Hardware: $hw, Software: $sw")
+
     }
   }
 }
